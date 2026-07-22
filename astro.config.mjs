c5560b371348ca6cab798f16b565d8ba62428f90
Kevin <kbarajas13@ucol.mx>
2026-07-22 03:36:26 -0600

    Implementacion del dominio oficial

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://novory-colima.github.io",
-  base: "/Novory/",
+  site: "https://novorystudio.com",
 
   vite: {
     plugins: [tailwindcss()],
